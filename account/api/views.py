27165146b7204c2b006c60fbb51d0ac7ba5dbc71
Shayan-9248 <shayan.aimoradii@gmail.com>
2022-02-12 16:44:27 +0330

    user list api view

--- account/api/views.py
+++ account/api/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth import get_user_model
 
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAdminUser,
+)
 
 from .serializers import (
     UserListSerializer,
@@ -13,9 +17,18 @@ User = get_user_model()
 
 
 class UserViewSet(ModelViewSet):
+    def get_permissions(self):
+        if self.action == ['list', 'retrieve']:
+            permission_classes = (IsAdminUser,)
+        else:
+            permission_classes = [IsAdminUser]
+        return [permission() for permission in permission_classes]
+
     def get_queryset(self):
         return User.objects.all()
-        
+
     def get_serializer_class(self):
         if self.action == 'list':
             return UserListSerializer
+        elif self.action == 'retrieve':
+            return UserRetrieveSerializer
