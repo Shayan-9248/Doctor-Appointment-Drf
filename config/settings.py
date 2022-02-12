81067972d6b909acd5898e049e4b5f160411b6fb
Shayan-9248 <shayan.aimoradii@gmail.com>
2022-02-12 18:57:18 +0330

    sign out api view

--- config/settings.py
+++ config/settings.py
@@ -142,8 +142,8 @@ AUTH_USER_MODEL = 'account.User'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "shayan.aimoradii@gmail.com"
-EMAIL_HOST_PASSWORD = "hvfyeqvqxmssayug"
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 EMAIL_PORT = 587
 
 # Rest Framework
